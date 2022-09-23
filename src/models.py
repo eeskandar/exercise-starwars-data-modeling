df85cd112ed79580ebe83b1f3bf41fde01d1252c
Iskandar <seik747@gmail.com>
2022-09-23 22:28:01 +0000

    cleaning

--- src/models.py
+++ src/models.py
@@ -51,32 +51,5 @@ class Favorites(Base):
     character_id = Column(Integer, ForeignKey('character.id'), nullable=True)
     planet_id = Column(Integer, ForeignKey('planet.id'), nullable=True)
 
-
-
-
-
-
-################ Old stuff ##################
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
-
-    def to_dict(self):
-        return {}
-
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
